136f60a4788723b0d6d8bef10a21c25198df7236
robertpitt <robertpitt1988@gmail.com>
2012-04-19 16:56:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Nothing to put here
---
+++ mining.js
@@ -0,0 +1,67 @@
+/*
+ * Google Profile Information Fun
+*/
+
+/*
+ * Require Libraries
+*/
+var http	= require('http');
+var url		= require('url');
+var fs		= require('fs');
+var locRegex = new RegExp("<loc>(.*?)</loc>","gi");
+var packages = [];
+
+/*
+ * Starting Link
+*/
+var baseOptions = {
+	host : 'www.gstatic.com',
+	port : 80,
+	path : '/s2/sitemaps/profiles-sitemap.xml'
+};
+
+http.get(baseOptions, function(response){
+	if(response.statusCode != 200)
+	{
+		console.error("Initial stack was not found");
+		process.exit();
+	};
+	
+	var stack = '';
+	response.on('data', function(chunk){
+		stack += chunk;
+	});
+
+	response.on('end', function(){
+		var match = null;
+
+		while(match = locRegex.exec(stack))
+		{
+			packages.push(match[1]);
+		}
+
+		processPackages();
+	});
+});
+
+var processPackages = function()
+{
+	var totalPackages = packages.length;
+	for(var i = 0; i < packages.length; i++)
+	{
+		var requestOptions = url.parse(packages[i]);
+
+		(function(index){ //Do not use i inside thise scope.. noob
+			http.get(requestOptions, function(response){
+				if(response.statusCode != 200)
+				{
+					console.log("failed to get package form google");
+					return;
+				}
+
+				response.pipe(fs.createWriteStream("./data/segment_" + index + ".txt"));
+				console.log("Piping " + index + " of " + totalPackages * index + " into ./data/segment_" + index + ".txt");
+			});
+		})(i);
+	}
+}
